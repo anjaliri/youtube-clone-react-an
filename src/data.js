4a0ca0cc7de70f57a2abd7ccf86eacbd6cb01ef9
anjaliri <anjalisikanya@gmail.com>
2024-05-02 16:30:56 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const API_KEY = "AIzaSyDn_c9lXmuIrz_ZgXeXiUMi4urqL2pdA3s";   
+
+export const value_converter = (value) => {
+    if(value>=1000000)
+    {
+        return Math.floor(value/1000000)+"M";
+    }
+    else if(value>=1000)
+    {
+        return Math.floor(value/1000)+"K";
+    }
+    else
+    {
+        return value;
+    }
+}
